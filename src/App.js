fff71defb44ae260b0021f94f4d482d6e579a807
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-18 02:08:25 +0100

    created li tags

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
     <div className="App">
       <h1>Testing How to host React</h1>
       <p>Will determine the progress later</p>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   );
 }
