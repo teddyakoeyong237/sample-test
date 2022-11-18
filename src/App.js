2af8d543d195b28ded730e05a11608147d255443
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-11-18 02:07:10 +0100

    modified app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Testing How to host React</h1>
       <p>Will determine the progress later</p>
+      <h3>They are just adding things for adding sake</h3>
     </div>
   );
 }
